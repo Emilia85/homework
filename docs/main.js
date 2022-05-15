56a3a5b548e88c656f955d0b6e05775951acec56
Emilia85 <emilia.ghinea85@yahoo.com>
2022-05-15 15:35:53 +0200

    dfhdg

--- docs/main.js
+++ docs/main.js
@@ -97,9 +97,11 @@ function sent(){
       "&color="+document.getElementById("color").value.substring(1)+
       "&voto="+document.getElementById("voto").value+
       "&messaggio="+document.getElementById("messaggio").value;
-      var finestra = window.open( "https://emilia85.github.io/Documents/homework/docs/new.html"+querystring,"width=200px,height=300px");
+      var finestra = window.open( "file:///Users/emiliag./Documents/Play/homework/docs/new.html"+querystring,"width=200px,height=300px");
       
       }
+      //https://emilia85.github.io/Documents/homework/docs/new.html
+      
       //https://emilia85.github.io/homework/new.html?sex=maschio&vacanza=mare&favcolor=%23fff76b&password=hjkhfhg&user-email=titi%40gmail.com&voto=10&messaggio=tresa&invia=Sent
       
  
